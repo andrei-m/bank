872b025df350dfbc4d97063ded3f05b0ded12980
Andrei Mackenzie <andreimackenzie@gmail.com>
2014-06-01 14:33:33 -0400

    start/end date filtering

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,7 +1,17 @@
-var app = angular.module('bank', []);
+var app = angular.module('bank', ['dateInput']);
 
 app.controller('transactionsList', function($scope, $http) {
     $http.get('/transactions').success(function(data) {
         $scope.transactions=data;
     });
+
+    $scope.endDate = new Date();
+    $scope.startDate = new Date();
+    $scope.startDate.setMonth($scope.startDate.getMonth() - 1);
+
+    $scope.dateFilter = function(transaction) {
+      var transDate = new Date(transaction.Date);
+      return transDate >= $scope.startDate && transDate <= $scope.endDate;
+    };
 });
+
---
+++ static/js/dateInput.js
@@ -0,0 +1,20 @@
+// Enable initialized inputs of type "date"
+angular.module('dateInput', [])
+  .directive(
+    'dateInput',
+    function(dateFilter) {
+        return {
+            require: 'ngModel',
+            template: '<input type="date"></input>',
+            replace: true,
+            link: function(scope, elm, attrs, ngModelCtrl) {
+                ngModelCtrl.$formatters.unshift(function (modelValue) {
+                    return dateFilter(modelValue, 'yyyy-MM-dd');
+                });
+
+                ngModelCtrl.$parsers.unshift(function(viewValue) {
+                    return new Date(viewValue);
+                });
+            },
+        };
+  });
